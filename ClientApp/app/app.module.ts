64082887c89d7b32b9cccef028638e517a0e9111
Scott Neu <scott.neu@fivepointsict.com>
2017-04-16 07:14:12 -0500

    Secured the UI layer Removed the universalmodule (server side rendering) Split out base routing

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,31 +1,44 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { UniversalModule } from 'angular2-universal';
-import { AppComponent } from './components/app/app.component'
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRouting } from './app.routing';
+
+// services
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
+
+// components
+import { AppComponent } from './components/app/app.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule,
+        AppRouting
+    ],
     declarations: [
         AppComponent,
+        LoginComponent,
+        HomeComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
     ],
-    imports: [
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+    providers: [
+        AuthService,
+        AuthGuard
+    ],
 })
 export class AppModule {
 }
---
+++ ClientApp/app/app.routing.ts
@@ -0,0 +1,23 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './services/auth.guard';
+
+// remove
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+
+    // remove
+    { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
+    { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const AppRouting = RouterModule.forRoot(appRoutes);
